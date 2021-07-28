3e3bc5ab96d6d3729291f2c953b94b2df59ba9e8
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-07-28 01:08:52 -0300

    Trabalhando com arquivos/ Manipulando pastas com File!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,35 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		//criando um vetor de string
-		//new serve para instanciar o vetor de string
-		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
+		Scanner sc = new Scanner(System.in);
 		
-		//criar o arquivo e gravar ele
-		//e informar o caminho do arquivo
-		String path = "C:\\Users\\jonas\\OneDrive\\Área de Trabalho\\out.txt";
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
 		
-		//fazer o try with resource
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
-			//fazer uma lógica para gravar as linhas
-			for(String line : lines) {
-				bw.write(line);
-				bw.newLine();//para quebrar a linha pois o write não há quebra de linha
-				
-			}
+		//craiar uma variável File 
+		File path = new File(strPath);
+		
+		//pegar todas as pastas
+		File[] folders =  path.listFiles(File::isDirectory);
+		System.out.println("Folders: ");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
-		catch(IOException e) {
-			e.printStackTrace(); 	 	
+		
+		//agora pegar os txt
+		File[] files = path.listFiles(File::isFile); 
+		for(File file : files) {
+			System.out.println(file);
 		}
+		
+		//criar uma subpasta
+		boolean success = new File(strPath + "\\Jonas").mkdir();
+		System.out.println("Directory created successfully: " + success);
 	}
 }
