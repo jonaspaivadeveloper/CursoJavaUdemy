952d19a7ecb0f0916b8980359c1caa46cbb49766
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-07-28 04:07:08 -0300

    Trabalhando com arquivos/ Informações do caminho do arquivo!

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,27 +9,13 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter a folder path: ");
+		System.out.print("Enter a file path: ");
 		String strPath = sc.nextLine();
 		
-		//craiar uma variável File 
 		File path = new File(strPath);
 		
-		//pegar todas as pastas
-		File[] folders =  path.listFiles(File::isDirectory);
-		System.out.println("Folders: ");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		//agora pegar os txt
-		File[] files = path.listFiles(File::isFile); 
-		for(File file : files) {
-			System.out.println(file);
-		}
-		
-		//criar uma subpasta
-		boolean success = new File(strPath + "\\Jonas").mkdir();
-		System.out.println("Directory created successfully: " + success);
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent " + path.getParent());
+		System.out.println("getPath " + path.getPath());
 	}
 }
