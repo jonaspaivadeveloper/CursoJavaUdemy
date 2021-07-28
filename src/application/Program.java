afa2b814ca00dd94688c1f0c4ff88ea679b8fbbd
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-07-27 23:46:34 -0300

    Trabalhando com arquivos/ FileWriter e BufferedWriter!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,33 +1,32 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-
-//Bloco try-with-resources
-//É um bloco try que declara um pu mais rexursos, e garante que 
-//esses recursos serão fechados ao final do bloco
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		String path = "c:\\in.txt";
+		//criando um vetor de string
+		//new serve para instanciar o vetor de string
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
 		
-		//para ler o arquivo
-		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
-			//manda uma linha do arquivo
-			String line = br.readLine();
-			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		//criar o arquivo e gravar ele
+		//e informar o caminho do arquivo
+		String path = "C:\\Users\\jonas\\OneDrive\\Área de Trabalho\\out.txt";
+		
+		//fazer o try with resource
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			//fazer uma lógica para gravar as linhas
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();//para quebrar a linha pois o write não há quebra de linha
+				
 			}
 		}
-		
 		catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}	
+			e.printStackTrace(); 	 	
+		}
 	}
 }
