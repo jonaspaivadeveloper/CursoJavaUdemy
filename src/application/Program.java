2b6e2bd4bf5b5c7f79c49240cf20b6e78b2d6862
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-08-07 22:18:43 -0300

    Interfaces/ Herdar vs. cumprir contrato 1!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,59 +1,25 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
 
-import entities.Contract;
-import entities.Installment;
-import services.ContractService;
-import services.PaypalService;
+
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.entities.Shape;
+import model.enums.Color;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
-		//OnlinePaymentService ps = new PaypalService(); //upcasting
+		Shape s1 = new Circle(Color.BLACK, 2.0);	
+		Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 		
-		//System.out.println(ps.paymentFee(200.00));
-		//System.out.println(ps.interest(200.00, 3));
+		System.out.println("Circle color: " + s1.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
 		
-		//OnlinePaymentService ps = new PaypalService();
-		//ContractService cs = new ContractService(ps);
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter contract data");
-		System.out.print("Number: ");
-		Integer number = sc.nextInt();
-		System.out.print("Date (dd/MM/yyyy): ");
-		Date date = sdf.parse(sc.next());
-		
-		System.out.print("Contract value: ");
-		Double totalValue = sc.nextDouble();
-		
-		Contract contract = new Contract(number, date, totalValue);
-		
-		System.out.print("Enter number of installments: ");
-		int N = sc.nextInt();
-		
-		ContractService cs = new ContractService(new PaypalService());
-		
-		cs.processContract(contract, N);
-		
-		System.out.println("Installments:");
-		for(Installment it : contract.getInstallments()) {
-			System.out.println(it);
-		}
-	
-		sc.close();
-		
-		
+		System.out.println("Rectangle color: " + s2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+
 	}
 
 }
