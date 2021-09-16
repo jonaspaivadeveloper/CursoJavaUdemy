a17d9e5e5d6e1c9ea548995c05a02cebb403f53b
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-09-16 18:13:25 -0300

    Generics, Set, Map/ Introdução aos Generics!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,31 @@
 package application;
 
-import java.util.Locale;
 import java.util.Scanner;
 
-import services.InterestService;
-import services.UsaInterestService;
+import services.PrintService;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Amount: ");
-		double amount = sc.nextDouble();
-		System.out.print("Months: ");
-		int months = sc.nextInt();
+		PrintService<Integer> ps = new PrintService<>();
 		
-		//é uma classe que instancia o brazilI.....
-		InterestService is = new UsaInterestService(1.0);
-		double payment = is.payment(amount, months);
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
 		
-		System.out.println("Payment after " + months + " months: " );
-		System.out.println(String.format("%.2f", payment));
+		for ( int  i = 0 ; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
 		
+		ps.print();
+		
+		Integer x = ps.first();
+		
+		System.out.println("First: " + ps.first());
 		sc.close();
-		
 	}
 
 }
