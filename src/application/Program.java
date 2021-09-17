7e61e90855d59d38e122f5bd1496259c902b6800
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-09-17 18:30:11 -0300

    Generics, Set, Map/ Tipos curinga!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,39 +1,22 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import entities.Product;
-import services.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(5, 2, 6);
+		printList(myInts);
 		
-		List<Product> list = new ArrayList<>();
-		
-		String path = "C:\\Users\\jonas\\OneDrive\\Área de Trabalho\\oi.txt";
-				
-		try (BufferedReader br = new BufferedReader( new FileReader(path))){
-			
-			String line = br.readLine();
-			while ( line != null) {
-				String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-				line = br.readLine();
-			}
-			
-			Product x = CalculationService.max(list);
-			System.out.println("Max: ");
-			System.out.println(x);
-			
-		} catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		List<String> myStrs = Arrays.asList("Jonas", "Helena", "Clara");
+		printList(myStrs);
+	}
+	
+	public static void printList(List<?> list) {
+		for(Object obj : list) {
+			System.out.println(obj);
 		}
-
 	}
 
 }
