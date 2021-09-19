a0b147f23db2c497b40572689c875cd1762dad5f
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-09-19 19:04:34 -0300

    Generics, Set, Map/ Como Set testa igualdade!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,54 +1,22 @@
 package application;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
-/*
-public class Program {
-
-	public static void main(String[] args) {
-		
-		//Set<String> set = new HashSet<>();
-		//Set<String> set = new TreeSet()<>();
-		Set<String> set = new LinkedHashSet<>();
-		 	
-		set.add("TV");
-		set.add("Notebook");
-		set.add("Tablet");
-		
-		//System.out.println(set.contains("Tablet"));
-		//set.remove("Tablet");
-		set.removeIf(x -> x.length() >= 3);
-		
-		for(String p : set) {
-			System.out.println(p);
-		}
-	}
-}
-*/
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
-		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 8, 9, 10));
-		
-		//union
-		Set<Integer> c = new TreeSet<>(a);
-		c.addAll(b);
-		System.out.println(c);
-		
-		//intersection
-		Set<Integer> d = new TreeSet<>(a);
-		d.retainAll(b);
-		System.out.println(d);
-		
-		//difference
-		Set<Integer> e = new TreeSet<>(a);
-		d.removeAll(b);
-		System.out.println(e);
-	}
-}
 	
+		Set<Product> set = new HashSet<>();
+		
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+		
+		Product prod = new Product("Notebook", 1200.0);
+		
+		System.out.println(set.contains(prod));
+	}
+}
