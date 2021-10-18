8536a5d5c189b86f7e775ecc9ca2dd04413c18f8
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-10-18 17:30:02 -0300

    Programação funcional e expressões lambda/ Cálculo de lambda parte 2!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,26 @@
 package application;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
 
 public class Program {
 	
-	public static int globalValue = 3;
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
 
 	public static void main(String[] args) {
 		
-		int[] vect = new int[] { 3, 4, 5};
-		changeOddValues(vect);
-		System.out.println(Arrays.toString(vect));	
-	}
-	
-	public static void changeOddValues(int[] numbers) {
-	for(int i = 0; i < numbers.length; i++) {
-		if (numbers[i] % 2 != 0) {
-			numbers[i] += globalValue;
-			
-			}
-		}
+		List<Product> list = new  ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 100.00));
+		
+		list.sort(Program :: compareProducts);
+		
+		list.forEach(System.out ::println);
 	}
 }
